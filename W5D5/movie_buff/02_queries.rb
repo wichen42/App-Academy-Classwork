c4937c7f5c167afe235c0c35ef92dd039578c0f1
Wilson Chen <wilsonchen901@gmail.com>
2022-08-19 22:01:02 -0400

    create Week 5 Day 5

---
+++ W5D5/movie_buff/02_queries.rb
@@ -0,0 +1,57 @@
+def eighties_b_movies
+  # List all the movies from 1980-1989 with scores falling between 3 and 5
+  # (inclusive). Show the id, title, year, and score.
+
+  Movie
+    .select('id, title, yr, score')
+    .where('score >= 3 AND score <= 5 AND yr BETWEEN 1980 AND 1989')
+
+end
+
+def bad_years
+  # List the years in which no movie with a rating above 8 was released.
+  Movie
+    .select(:yr)
+    .where('yr NOT IN (:bad)', bad: Movie.select('yr').where('score > 8'))
+    .group(:yr)
+    .pluck(:yr)
+
+end
+
+def cast_list(title)
+  # List all the actors for a particular movie, given the title.
+  # Sort the results by starring order (ord). Show the actor id and name.
+  
+  Actor
+    .select('actors.id, name')
+    .joins(:movies)
+    .where('title = ?', title)
+    .order(ord: :asc)
+
+end
+
+def vanity_projects
+  # List the title of all movies in which the director also appeared as the
+  # starring actor. Show the movie id, title, and director's name.
+
+  # Note: Directors appear in the 'actors' table.
+
+  Movie
+    .select('movies.id, title, name')
+    .joins(:actors)
+    .where('director_id = actor_id AND ord = 1')
+
+end
+
+def most_supportive
+  # Find the two actors with the largest number of non-starring roles.
+  # Show each actor's id, name, and number of supporting roles.
+  
+  Actor
+    .select('actors.id, name, count(castings.movie_id) AS roles')
+    .joins(:movies)
+    .where('ord > 1')
+    .group('actors.id')
+    .order(roles: :desc)
+    .limit(2)
+end
