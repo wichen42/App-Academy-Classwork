eec8cbeb67f4acddd8db5027013a459e5e9c93dd
Wilson Chen <wilsonchen901@gmail.com>
2022-10-05 14:34:29 -0400

    create week 12 day 3 benchbnb

---
+++ W12D3/BenchBnB/frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+// import LoginFormPage from "./components/LoginFormPage";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+        <Switch>
+          {/* <Route path="/login" >
+            <LoginFormPage />
+          </Route> */}
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+    </>
+  );
+}
+
+export default App;
